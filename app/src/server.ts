402a964dd727e31532b89ff52163e13639cb8291
Coodos <info@coodos.co>
2022-11-25 06:39:45 +0530

    chore: remove useless run

--- app/src/server.ts
+++ app/src/server.ts
@@ -13,22 +13,3 @@ app.use("/api", router);
 app.listen(3000, () => {
     console.log("server listening on port 3000");
 });
-
-async function run() {
-    const map = (
-        await fs.readFile(
-            "/Users/merul/Desktop/Projects/code-blooded-p3/app/src/data/sample_3/mapping.csv"
-        )
-    ).toString();
-    const src = JSON.parse(
-        (
-            await fs.readFile(
-                "/Users/merul/Desktop/Projects/code-blooded-p3/app/src/data/sample_3/source.json"
-            )
-        ).toString()
-    );
-    console.log(src, map);
-    console.log(transformObject(src, map));
-}
-
-run();
